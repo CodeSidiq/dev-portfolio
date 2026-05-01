d62713ec5bde932e4acb84b4da390927fc2e16c8
codesidiq <sidiqkusumah99@gmail.com>
2026-05-01 14:20:47 +0700

    fix: suppress hydration warning from browser injected attributes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={clsx(poppins.variable, 'antialiased')}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={clsx(poppins.variable, 'antialiased')} suppressHydrationWarning>{children}</body>
     </html>
   );
 }
